00adf88d58a8877c44e19691a92913201bb08444
junga970 <junga2940@gmail.com>
2025-07-14 16:22:57 +0900

    user client 연동

--- comment-service/src/main/java/com/playvoice/commentservice/command/repository/CommentRepository.java
+++ comment-service/src/main/java/com/playvoice/commentservice/command/repository/CommentRepository.java
@@ -3,7 +3,6 @@ package com.playvoice.commentservice.command.repository;
 import com.playvoice.commentservice.command.entity.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +10,6 @@ import org.springframework.stereotype.Repository;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     Page<Comment> findByPostId(Long postId, Pageable pageable);
+
+    Long countByPostId(Long postId);
 }
