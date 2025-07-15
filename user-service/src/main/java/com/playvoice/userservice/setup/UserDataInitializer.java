0413a1f12c19c564ff2f5c98d7d5f3a4c0bad1fa
binipk <mingyoku93@gmail.com>
2025-07-15 15:46:38 +0900

    initial user is added

--- user-service/src/main/java/com/playvoice/userservice/setup/UserDataInitializer.java
+++ user-service/src/main/java/com/playvoice/userservice/setup/UserDataInitializer.java
@@ -5,6 +5,8 @@ import com.playvoice.userservice.entity.User;
 import com.playvoice.userservice.entity.UserRole;
 import com.playvoice.userservice.repository.UserRepository;
 import java.time.LocalDateTime;
+import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,11 +22,14 @@ public class UserDataInitializer implements CommandLineRunner {
     @Override
     public void run(String... args) {
         if (userRepository.count() == 0) {
-            User user = User.builder()
+
+            List<User> user = List.of(
+
+            User.builder()
                 .email("manager1@example.com")
                 .username("manager1")
                 .password(passwordEncoder.encode("manager1"))
-                .name("Manager1")
+                .name("박서현")
                 .role(UserRole.MANAGER)
                 .course("Manager")
                 .isBanned(false)
@@ -32,9 +37,42 @@ public class UserDataInitializer implements CommandLineRunner {
                 .createdAt(LocalDateTime.now())
                 .lastChangedPassword(LocalDateTime.now())
                 .lastLogin(LocalDateTime.now())
-                .build();
+                .build(),
 
-            userRepository.save(user);
+            // -------------------------------------------------------------------
+
+            User.builder()
+                    .email("student1@example.com")
+                    .username("student1")
+                    .password(passwordEncoder.encode("student1"))
+                    .name("만수르")
+                    .role(UserRole.STUDENT)
+                    .course("Full-Stack The 1st")
+                    .isBanned(false)
+                    .passwordStatus(PasswordStatus.INIT)
+                    .createdAt(LocalDateTime.now())
+                    .lastChangedPassword(LocalDateTime.now())
+                    .lastLogin(LocalDateTime.now())
+                    .build(),
+
+            // -------------------------------------------------------------------
+
+            User.builder()
+                    .email("student2@example.com")
+                    .username("student2")
+                    .password(passwordEncoder.encode("student2"))
+                    .name("트럼프")
+                    .role(UserRole.STUDENT)
+                    .course("Full-Stack The 1st")
+                    .isBanned(false)
+                    .passwordStatus(PasswordStatus.INIT)
+                    .createdAt(LocalDateTime.now())
+                    .lastChangedPassword(LocalDateTime.now())
+                    .lastLogin(LocalDateTime.now())
+                    .build()
+            );
+
+            userRepository.saveAll(user);
             System.out.println("User Data Inserted");
         }
     }
